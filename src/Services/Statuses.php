e95c353ea939c722de6ab3391a071059fbfd988b
Andrey Helldar <helldar@ai-rus.com>
2021-08-08 21:58:20 +0300

    Upgrade Statuses contract

--- src/Services/Statuses.php
+++ src/Services/Statuses.php
@@ -19,8 +19,11 @@ declare(strict_types=1);
 
 namespace Helldar\Cashier\Services;
 
+use Helldar\Cashier\Constants\Status;
+use Helldar\Cashier\Facades\Config\Payment;
 use Helldar\Contracts\Cashier\Helpers\Statuses as Contract;
 use Helldar\Support\Concerns\Makeable;
+use Helldar\Support\Facades\Helpers\Arr;
 use Illuminate\Database\Eloquent\Model;
 
 /** @method static Statuses make(Model $model) */
@@ -46,64 +49,94 @@ abstract class Statuses implements Contract
         $this->model = $model;
     }
 
-    public function hasUnknown(string $status = null): bool
+    public function hasUnknown($status = null): bool
     {
-        $status = $status ?: $this->status();
-
-        return ! $this->has(array_merge([
+        $bank = array_merge([
             static::NEW,
             static::REFUNDING,
             static::REFUNDED,
             static::FAILED,
             static::SUCCESS,
-        ]), $status);
+        ]);
+
+        $model = [
+            Status::NEW,
+            Status::WAIT_REFUND,
+            Status::REFUND,
+            Status::FAILED,
+            Status::SUCCESS,
+        ];
+
+        return ! $this->has($bank, $status)
+            && ! $this->hasModel($model, $status);
     }
 
-    public function hasCreated(string $status = null): bool
+    public function hasCreated($status = null): bool
     {
-        $status = $status ?: $this->status();
-
-        return $this->has(static::NEW, $status);
+        return $this->has(static::NEW, $status)
+            || $this->hasModel([Status::NEW], $status);
     }
 
-    public function hasFailed(string $status = null): bool
+    public function hasFailed($status = null): bool
     {
-        return $this->has(static::FAILED, $status);
+        return $this->has(static::FAILED, $status)
+            || $this->hasModel([Status::FAILED], $status);
     }
 
-    public function hasRefunding(string $status = null): bool
+    public function hasRefunding($status = null): bool
     {
-        return $this->has(static::REFUNDING, $status);
+        return $this->has(static::REFUNDING, $status)
+            || $this->hasModel([Status::WAIT_REFUND], $status);
     }
 
-    public function hasRefunded(string $status = null): bool
+    public function hasRefunded($status = null): bool
     {
-        return $this->has(static::REFUNDED, $status);
+        return $this->has(static::REFUNDED, $status)
+            || $this->hasModel([Status::REFUND], $status);
     }
 
-    public function hasSuccess(string $status = null): bool
+    public function hasSuccess($status = null): bool
     {
-        return $this->has(static::SUCCESS, $status);
+        return $this->has(static::SUCCESS, $status)
+            || $this->hasModel([Status::SUCCESS], $status);
     }
 
-    public function inProgress(string $status = null): bool
+    public function inProgress($status = null): bool
     {
         return $this->hasCreated($status) || $this->hasRefunding($status);
     }
 
-    protected function has(array $statuses, string $status = null): bool
+    protected function has(array $statuses, $status = null): bool
     {
-        $status = $status ?: $this->status();
+        $status = ! is_null($status) ? $status : $this->cashierStatus();
 
-        return ! empty($status) && in_array($status, $statuses, true);
+        return ! is_null($status) && in_array($status, $statuses, true);
     }
 
-    protected function status(): ?string
+    protected function hasModel(array $statuses, $status = null): bool
+    {
+        $statuses = Arr::map($statuses, static function (string $status) {
+            return Payment::getStatuses()->getStatus($status);
+        });
+
+        $status = ! is_null($status) ? $status : $this->modelStatus();
+
+        return $this->has($statuses, $status);
+    }
+
+    protected function cashierStatus(): ?string
     {
         if ($this->model->cashier && $this->model->cashier->details) {
             return $this->model->cashier->details->getStatus();
         }
 
         return null;
+    }
+
+    protected function modelStatus()
+    {
+        $field = Payment::getAttributes()->getStatus();
+
+        return $this->model->getAttribute($field);
     }
 }
