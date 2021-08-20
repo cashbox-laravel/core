42eef5db76856e2b5c7ee508d68f017762798a33
Andrey Helldar <andrey-helldar@users.noreply.github.com>
2021-08-20 13:34:53 +0000

    Apply fixes from StyleCI

--- src/Resources/Model.php
+++ src/Resources/Model.php
@@ -41,14 +41,6 @@ abstract class Model implements Contract
         $this->config = $config;
     }
 
-    abstract protected function paymentId();
-
-    abstract protected function sum();
-
-    abstract protected function currency();
-
-    abstract protected function createdAt(): Carbon;
-
     public function getClientId(): string
     {
         return $this->clientId();
@@ -92,6 +84,14 @@ abstract class Model implements Contract
     {
         return $this->model->cashier->external_id ?? null;
     }
+
+    abstract protected function paymentId();
+
+    abstract protected function sum();
+
+    abstract protected function currency();
+
+    abstract protected function createdAt(): Carbon;
 
     protected function clientId(): string
     {
