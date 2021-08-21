a751d5aa17fa428bb0ef3ced43c4ed70fb6aa644
Andrey Helldar <helldar@ai-rus.com>
2021-08-21 21:12:31 +0300

    Added new exceptions

---
+++ src/Exceptions/Http/TooManyRequestsException.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the "andrey-helldar/cashier" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/cashier
+ */
+
+namespace Helldar\Cashier\Exceptions\Http;
+
+class TooManyRequestsException extends BaseException
+{
+    protected $status_code = 429;
+
+    protected $reason = 'Too Many Requests';
+}
+
