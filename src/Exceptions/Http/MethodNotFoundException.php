99097741873990f18b3ebfda7d5846e82d13e348
Andrey Helldar <helldar@ai-rus.com>
2021-08-24 18:23:56 +0300

    Extra dock removed

--- src/Exceptions/Http/MethodNotFoundException.php
+++ src/Exceptions/Http/MethodNotFoundException.php
@@ -17,21 +17,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the "andrey-helldar/cashier" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@ai-rus.com>
- *
- * @copyright 2021 Andrey Helldar
- *
- * @license MIT
- *
- * @see https://github.com/andrey-helldar/cashier
- */
-
 namespace Helldar\Cashier\Exceptions\Http;
 
 class MethodNotFoundException extends BaseException
