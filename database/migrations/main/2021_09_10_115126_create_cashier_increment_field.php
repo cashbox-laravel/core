28dcf552e3a3cc9de564e6b56c3e32487ee1d129
Andrey Helldar <helldar@ai-rus.com>
2021-09-10 12:00:02 +0300

    Added an incremental field to the data table

---
+++ database/migrations/main/2021_09_10_115126_create_cashier_increment_field.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the "andrey-helldar/cashier" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/cashier
+ */
+
+declare(strict_types=1);
+
+use Helldar\Cashier\Facades\Config\Details;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCashierIncrementField extends Migration
+{
+    public function up()
+    {
+        Schema::table($this->table(), function (Blueprint $table) {
+            $table->id()->primary()->first();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table($this->table(), function (Blueprint $table) {
+            $table->dropColumn('id');
+        });
+    }
+
+    protected function table(): string
+    {
+        return Details::getTable();
+    }
+}
