bf96f8e2d038e9c2632fc097a96c4a9a2ff5de6a
Andrey Helldar <helldar@dragon-code.pro>
2023-07-15 02:17:08 +0300

    Added configuration tests

--- src/Data/Config/ConfigData.php
+++ src/Data/Config/ConfigData.php
@@ -22,6 +22,7 @@ use Cashbox\Core\Data\Config\Queue\QueueData;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
 
 class ConfigData extends Data
 {
@@ -42,7 +43,7 @@ class ConfigData extends Data
     public RefundData $refund;
 
     #[DataCollectionOf(DriverData::class)]
-    public array $drivers;
+    public DataCollection $drivers;
 
     public function payment(): PaymentData
     {
