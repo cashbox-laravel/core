6e2ffa01b030fa49e388fa760bc92a2701483a72
Andrey Helldar <helldar@dragon-code.pro>
2023-08-04 23:11:35 +0300

    Added new exceptions

---
+++ src/Exceptions/External/BankInternalErrorHttpException.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the "cashbox/foundation" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2023 Andrey Helldar
+ * @license MIT
+ *
+ * @see https://cashbox.city
+ */
+
+declare(strict_types=1);
+
+namespace Cashbox\Core\Exceptions\External;
+
+use Cashbox\Core\Exceptions\BaseException;
+
+class BankInternalErrorHttpException extends BaseException
+{
+    protected int $statusCode = 400;
+
+    protected string $reason = 'Bank internal error';
+}
